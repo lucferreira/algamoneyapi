ec7a6b6be4021d435e37a4d6230e82441a6a3fab
lucferreira <luciano.ferreira0@gmail.com>
2019-07-23 21:39:43 -0300

    [new] classe lancamento

---
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/model/Lancamento.java
@@ -0,0 +1,13 @@
+package com.algaworks.algamoneyapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "lancamento")
+public class Lancamento {
+	
+	private Long codlancamento;
+	
+
+}
